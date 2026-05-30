33de5ab2dd9f7e648e9bdb10c93fb2542bdf1a4e
Julien Joseph Thomas <89767355+julien-kv@users.noreply.github.com>
2026-05-30 17:02:57 +0530

    Update padding in EventDetailView: Adjusted the padding in the event detail view to account for bottom media query padding, improving layout responsiveness on devices with varying screen sizes.

--- lib/features/updates/views/event_detail_view.dart
+++ lib/features/updates/views/event_detail_view.dart
@@ -109,7 +109,7 @@ class _EventDetailViewState extends State<EventDetailView> {
           ),
           SliverToBoxAdapter(
             child: Padding(
-              padding: const EdgeInsets.all(20),
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 20 + MediaQuery.of(context).padding.bottom),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
